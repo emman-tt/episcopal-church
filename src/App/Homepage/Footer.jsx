e6708010b8ef618ee7ef088c24aca6fc0adc2275
Your Name <Emmanuelaquarius2006@gmail.com>
2026-02-26 01:05:41 +0000

    created some paths

--- src/App/Homepage/Footer.jsx
+++ src/App/Homepage/Footer.jsx
@@ -3,7 +3,8 @@ import { Logo } from '../../components/Logo'
 import x from '../../assets/img/icons/x.svg'
 import facebook from '../../assets/img/icons/facebook.svg'
 import github from '../../assets/img/icons/github.svg'
-export default function Footer() {
+import { NavLink } from 'react-router-dom'
+export default function Footer () {
   return (
     <section className='flex flex-col p-6 md:p-30 pb-10 min-h-max md:min-h-120 mt-20 md:mt-30 bg-black text-white'>
       <header className='flex flex-col md:flex-row w-full items-start md:items-center justify-between gap-10'>
@@ -31,8 +32,26 @@ export default function Footer() {
           <li className='flex flex-col gap-4 text-xs md:text-sm'>
             <h3 className='text-lg md:text-xl font-semibold'>Quick Links</h3>
             <p className='cursor-pointer hover:text-[#8f3337]'>Home</p>
+            <NavLink
+              to={'/about'}
+              className='cursor-pointer hover:text-[#8f3337]'
+            >
+              About
+            </NavLink>
+            <NavLink
+              to={'/arts'}
+              className='cursor-pointer  hover:text-[#8f3337]'
+            >
+              Arts & Media
+            </NavLink>
+            <NavLink
+              to={'/verses'}
+              className='cursor-pointer hover:text-[#8f3337]'
+            >
+              Bible verses
+            </NavLink>
             <p className='cursor-pointer hover:text-[#8f3337]'>Congregation</p>
-            <p className='cursor-pointer hover:text-[#8f3337]'>Gallery</p>
+
             <p className='cursor-pointer hover:text-[#8f3337]'>Contact</p>
           </li>
           <li className='flex gap-4 flex-col text-xs md:text-sm'>
@@ -51,7 +70,9 @@ export default function Footer() {
             </div>
           </li>
           <li className='flex flex-col w-full md:w-[40%] col-span-1 sm:col-span-2 md:col-span-1 border-t border-gray-800 md:border-t-0 pt-10 md:pt-0'>
-            <h3 className='text-2xl md:text-3xl font-semibold'>Newsletter Signup</h3>
+            <h3 className='text-2xl md:text-3xl font-semibold'>
+              Newsletter Signup
+            </h3>
             <div className='py-4 md:py-5 mt-2 md:mt-3 justify-between w-full flex gap-5 border-b border-gray-700'>
               <input
                 type='text'
@@ -70,12 +91,20 @@ export default function Footer() {
       </main>
 
       <article className='w-full flex flex-col md:flex-row text-[10px] md:text-xs border-t-gray-800 pt-8 border-t justify-between items-center mt-10 gap-6 md:gap-0'>
-        <p className='text-center md:text-left'>Copyright @ 2025 Gracecarthage. All rights reserved.</p>
-        <p className='text-center md:text-left'>Built by Emmanuel Acquah at ReyoTechLabs</p>
+        <p className='text-center md:text-left'>
+          Copyright @ 2025 Gracecarthage. All rights reserved.
+        </p>
+        <p className='text-center md:text-left'>
+          Built by Emmanuel Acquah at ReyoTechLabs
+        </p>
         <div className='flex gap-8 md:gap-10 cursor-pointer text-white'>
           <img src={x} className='h-4 w-4 md:h-5 md:w-5' alt='x' />
           <img src={github} className='h-4 w-4 md:h-5 md:w-5' alt='github' />
-          <img src={facebook} className='h-4 w-4 md:h-5 md:w-5' alt='facebook' />
+          <img
+            src={facebook}
+            className='h-4 w-4 md:h-5 md:w-5'
+            alt='facebook'
+          />
         </div>
       </article>
     </section>
